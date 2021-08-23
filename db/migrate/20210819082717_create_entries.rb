ea4b3c5fb513e24034ded8b7ff69d4f37bb4adbf
Iryna Audzei <avirene@live.com>
2021-08-23 17:06:29 +0100

    "Created DB migration"

--- db/migrate/20210819082717_create_entries.rb
+++ db/migrate/20210819082717_create_entries.rb
@@ -1,7 +1,7 @@
 class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
-      t.string : word
+      t.string :word
       t.string :pronunciation
       t.text :definition
       t.text :example
